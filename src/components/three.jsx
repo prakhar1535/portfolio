e9915d6d1236709949f4b0695eb55f7a96521149
Prakhar Sharma <113304996+prakhar1535@users.noreply.github.com>
2023-10-31 15:20:55 +0530

    i

--- src/components/three.jsx
+++ src/components/three.jsx
@@ -10,7 +10,7 @@ z-index: -9999:
 export default function App() {
   return (
     <Background> 
-    <Spline style={{position: 'absolute', zIndex: '-1' , padding: '0', margin: '0', width: '100%' , height: '100%', top: '0', left: '0'}} scene="https://prod.spline.design/PkHBaT7Rc66IdCfN/scene.splinecode" />
+    <Spline style={{position: 'absolute', zIndex: '-1' , padding: '0', margin: '0', width: '100%' , height: '100%', top: '0', left: '0.1'}} scene="https://prod.spline.design/PkHBaT7Rc66IdCfN/scene.splinecode" />
     </Background>
   );
 }
